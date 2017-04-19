616d3d47b99ca403d35b7813044934a83bf4ac15
James <jwin4740@gmail.com>
2017-04-19 11:12:42 -0400

    user model creation

---
+++ models/user.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("users", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    hash: {
+      type: DataTypes.STRING
+    },
+    salt: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'users'
+  }
+  );
+  return User;
+};
+
